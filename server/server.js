22f38bb3acd8d4d9faadeded51e2178409c8c65f
Tarnell <richard.tarnell@gmail.com>
2021-11-03 19:35:56 -0600

    Latest version of server

--- server/server.js
+++ server/server.js
@@ -14,9 +14,22 @@ app.get('/api/hello', (req, res) => {
 
 app.get('/api/getImages', (req, res) => {
   const assets = fs.readdirSync('./images');
+  const imageURLs = [];
 
-  const fullpath = path.join(__dirname, `./images/${assets[req.query.asset]}`);
-  res.sendFile(fullpath);
+  const { page, limit } = req.query;
+
+  const start = parseInt(page, 10) * 10;
+  const end = start + parseInt(limit, 10);
+
+  for (let i = start; i < end; i += 1) {
+    const fullpath = path.join(__dirname, `./images/${assets[end]}`);
+
+    imageURLs.push(fullpath);
+  }
+
+  const responseObject = { urls: imageURLs };
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(responseObject));
 });
 
 app.listen(port, () => {
